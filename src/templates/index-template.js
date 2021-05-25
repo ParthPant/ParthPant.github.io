adbf5316edc746085555a723c257ef3b600b3c54
Parth Pant <parthpant4@gmail.com>
2021-05-25 10:01:24 +0530

    removed Link form index-template

--- src/templates/index-template.js
+++ src/templates/index-template.js
@@ -1,6 +1,6 @@
 // @flow strict
 import React from 'react';
-import { graphql, Link } from 'gatsby';
+import { graphql} from 'gatsby';
 import Layout from '../components/Layout';
 import Sidebar from '../components/Sidebar';
 import Feed from '../components/Feed';
